82a5ccfa4cea5945fddee1ad07f16a0156e3644d
Antonin <antoninlefevre45@icloud.com>
2022-08-17 22:04:07 +0200

    layout

--- pages/dataset.py
+++ pages/dataset.py
@@ -1,11 +1,5 @@
-import base64
-import datetime
-import io
-import dash
-from dash.dependencies import Input, Output, State
 from dash import dcc, html, dash_table, callback
 import dash_bootstrap_components as dbc
-import pandas as pd
 
 jumbotron = html.Div(
     dbc.Container(
@@ -23,80 +17,47 @@ jumbotron = html.Div(
     className="p-3 rounded-3",
 )
 
+form_dataset = dbc.Form(dbc.Row(
+    [
+        dbc.Col(
+            dbc.RadioItems(
+                id="example-radios-row",
+                options=[
+                    {"label": "Iris", "value": "Iris"},
+                    {"label": "Penguins", "value": "Penguins"},
+                    {"label": "Car Price", "value": "CarPrice"},
+                ],
+            ),
+            width=10,
+        ),
+    ],
+    className="mb-3",
+))
+
+accordion = html.Div(
+    dbc.Accordion(
+        [
+            dbc.AccordionItem(
+                [
+                    form_dataset
+                ],
+                title="Choisir un dataset",
+            )
+        ],
+    )
+)
+
 layout = html.Div([
     dbc.Row([
         dbc.Col(width=1),
         dbc.Col([
-            html.Br(), html.Br(), html.Br(), jumbotron,
+            html.Br(), html.Br(), html.Br(), jumbotron
         ], width=8)
     ]),
-    html.Div([
-        dcc.Upload(
-            id='upload-data',
-            children=html.Div([
-                'Drag and Drop or ',
-                html.A('Select Files')
-            ]),
-            style={
-                'width': '100%',
-                'height': '60px',
-                'lineHeight': '60px',
-                'borderWidth': '1px',
-                'borderStyle': 'dashed',
-                'borderRadius': '5px',
-                'textAlign': 'center',
-                'margin': '10px'
-            },
-        ),
-        html.Div(id='output-data-upload'),
+    dbc.Row([
+        dbc.Col(width=1),
+        dbc.Col([
+            html.Br(), accordion
+        ], width=5)
     ])
 ])
-
-
-def parse_contents(contents, filename, date):
-    content_type, content_string = contents.split(',')
-
-    decoded = base64.b64decode(content_string)
-    try:
-        if 'csv' in filename:
-            # Assume that the user uploaded a CSV file
-            df = pd.read_csv(
-                io.StringIO(decoded.decode('utf-8')))
-        elif 'xls' in filename:
-            # Assume that the user uploaded an excel file
-            df = pd.read_excel(io.BytesIO(decoded))
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-        ])
-
-    return html.Div([
-        html.H5(filename),
-        html.H6(datetime.datetime.fromtimestamp(date)),
-
-        dash_table.DataTable(
-            df.to_dict('records'),
-            [{'name': i, 'id': i} for i in df.columns]
-        ),
-
-        html.Hr(),  # horizontal line
-
-        # For debugging, display the raw contents provided by the web browser
-        html.Div('Raw Content'),
-        html.Pre(contents[0:200] + '...', style={
-            'whiteSpace': 'pre-wrap',
-            'wordBreak': 'break-all'
-        })
-    ])
-
-@callback(Output('output-data-upload', 'children'),
-              Input('upload-data', 'contents'),
-              State('upload-data', 'filename'),
-              State('upload-data', 'last_modified'))
-def update_output(list_of_contents, list_of_names, list_of_dates):
-    if list_of_contents is not None:
-        children = [
-            parse_contents(c, n, d) for c, n, d in
-            zip(list_of_contents, list_of_names, list_of_dates)]
-        return children
